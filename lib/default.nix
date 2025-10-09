35be3bae173041570332a7d3c98e2ccee8bee209
Daniel-De-Dev <id@daniel-de-dev.com>
2025-10-09 21:56:21 +0200

    refactor(lib): Add robust validation to mkHostConfigurations
    The `mkHostConfigurations` function previously assumed its inputs and
    the host directory structure were correct. (would not give good errors)
    
    This commit adds checks to make the function more robust:
    
    - Throws a clear error if the `hostDir` does not exist.
    - Asserts that the `modules` argument is a list.
    - Verifies that each host has the required `meta.nix` and
      `configuration.nix` files, providing a error if not.

--- lib/default.nix
+++ lib/default.nix
@@ -3,22 +3,47 @@ let
   inherit (inputs.nixpkgs) lib;
 
   listHostNames =
-    dir: builtins.attrNames (lib.filterAttrs (_: type: type == "directory") (builtins.readDir dir));
+    dir:
+    let
+      entries =
+        if builtins.pathExists dir then
+          builtins.readDir dir
+        else
+          throw "mkHostConfigurations: host directory ${toString dir} does not exist";
+    in
+    builtins.attrNames (lib.filterAttrs (_: type: type == "directory") entries);
 
   mkHostConfigurations =
     {
       hostDir ? ../hosts,
       modules ? [ ],
     }:
+    let
+      moduleList =
+        assert lib.assertMsg (lib.isList modules)
+          "mkHostConfigurations: `modules` must be a list of NixOS modules.";
+        modules;
+    in
     lib.genAttrs (listHostNames hostDir) (
       hostName:
       let
-        hostMetaModule = import (hostDir + "/${hostName}" + "/meta.nix");
-        hostMainModule = import (hostDir + "/${hostName}" + "/configuration.nix");
+        hostPath = hostDir + "/${hostName}";
+
+        requireFile =
+          hPath: fileName:
+          let
+            filePath = hPath + "/${fileName}";
+          in
+          if builtins.pathExists filePath then
+            filePath
+          else
+            throw "mkHostConfigurations: expected ${fileName} for host '${hostName}' at ${toString filePath}";
+        hostMetaModule = import (requireFile hostPath "meta.nix");
+        hostMainModule = import (requireFile hostPath "configuration.nix");
       in
       inputs.nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs hostName; };
-        modules = modules ++ [
+        modules = moduleList ++ [
           hostMetaModule
           hostMainModule
         ];
