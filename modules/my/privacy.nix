5b1b624bd05aa2eb36d2f2daabbabb4278def80f
Daniel-De-Dev <id@daniel-de-dev.com>
2025-10-18 17:14:37 +0200

    fix(privacy): Fixed ReadOnly attribute
    Turns out if you were to disable the privacy modules you'd get and error
    due to `my.privacy.data` not being defined resulting OR assigments of values
    to fail and throw errors.
    
    This fixes the problem so that its always defined, either with
    the actuall data or simply just `{}` when disabled.

--- modules/my/privacy.nix
+++ modules/my/privacy.nix
@@ -20,39 +20,45 @@
     };
   };
 
-  config = lib.mkIf config.my.privacy.enable {
+  config = {
     my.privacy.data =
-      let
-        privacyRoot =
-          if inputs ? privacy then
-            inputs.privacy
+      # It's filled with data when enabled otherwise set to `{}`
+      if config.my.privacy.enable then
+        (
+          let
+            privacyRoot =
+              if inputs ? privacy then
+                inputs.privacy
+              else
+                builtins.throw ''
+                  my.privacy.enable is true, but the 'privacy' flake input is not defined.
+                  Please add the 'privacy' input to your flake.nix or set my.privacy.enable = false for host "${hostName}".
+                '';
+
+            hostPrivacyFile = privacyRoot + "/hosts/${hostName}.nix";
+
+            rawPrivacy =
+              if builtins.pathExists hostPrivacyFile then
+                let
+                  imported = import hostPrivacyFile;
+                in
+                if builtins.isFunction imported then imported { inherit hostName; } else imported
+              else
+                builtins.throw ''
+                  my.privacy.enable is true, but the privacy file was not found for host "${hostName}".
+                  Nix looked for the file at the following path:
+                  ${hostPrivacyFile}
+                '';
+          in
+          if builtins.isAttrs rawPrivacy then
+            rawPrivacy
           else
             builtins.throw ''
-              my.privacy.enable is true, but the 'privacy' flake input is not defined.
-              Please add the 'privacy' input to your flake.nix or set my.privacy.enable = false for host "${hostName}".
-            '';
-
-        hostPrivacyFile = privacyRoot + "/hosts/${hostName}.nix";
-
-        rawPrivacy =
-          if builtins.pathExists hostPrivacyFile then
-            let
-              imported = import hostPrivacyFile;
-            in
-            if builtins.isFunction imported then imported { inherit hostName; } else imported
-          else
-            builtins.throw ''
-              my.privacy.enable is true, but the privacy file was not found for host "${hostName}".
-              Nix looked for the file at the following path:
-              ${hostPrivacyFile}
-            '';
-      in
-      if builtins.isAttrs rawPrivacy then
-        rawPrivacy
+              The privacy file for host "${hostName}" did not return an attribute set.
+              Please ensure the file content is in the format `{ <key> = <value>; }`.
+            ''
+        )
       else
-        builtins.throw ''
-          The privacy file for host "${hostName}" did not return an attribute set.
-          Please ensure the file content is in the format `{ key = "value"; }`.
-        '';
+        { };
   };
 }
